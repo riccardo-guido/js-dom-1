9fb8bb660d70bc22b86a679861ebeb2211d91aa1
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-09 16:44:51 +0200

    bonus-completed

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ const powerButton = document.getElementById("power-button");
 const imgEl = document.getElementById("img-element");
 
 
-powerButton.addEventListener("click", function() {
-    imgEl.src ="./img/yellow_lamp.png"
-    
-})
-
+powerButton.addEventListener('click', function() {
+    if (powerButton .textContent.includes('Accendi')) {
+      imgEl.src = './img/yellow_lamp.png';
+      powerButton.textContent = 'Spegni';
+    } else {
+      imgEl.src = './img/white_lamp.png';
+      powerButton.textContent = 'Accendi';
+    }
+  });
 
 
