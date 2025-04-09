88a67f5104f95c6b34d18f3d9876f51adac7fe45
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-09 16:33:11 +0200

    click-change-function

---
+++ script.js
@@ -0,0 +1,11 @@
+const powerButton = document.getElementById("power-button");
+const imgEl = document.getElementById("img-element");
+
+
+powerButton.addEventListener("click", function() {
+    imgEl.src ="./img/yellow_lamp.png"
+    
+})
+
+
+
